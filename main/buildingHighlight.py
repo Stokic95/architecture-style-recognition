ec86ed240a9c3967880124433ed9c588971c87ac
Stokic95 <sretenstokic@hotmail.com>
2018-02-11 23:14:43 +0100

    commenting

--- main/buildingHighlight.py
+++ main/buildingHighlight.py
@@ -3,9 +3,13 @@ import numpy as np
 
 
 def open_grayscale_image(_image_path):
+    # otvara i vraca crno-bijelu sliku
     return cv2.imread(_image_path, 0)
 
 def make_histogram(_image_path):
+
+    # kreira histogram koji moze da pokaze koliko je kojih piksela na slici
+    # vrati listu od 256 jednoclanih lista, u svakoj listi je broj koji pokazuje koliko ima nekog piksela
 
     # open picture
     # param 1 - path to image
@@ -25,6 +29,10 @@ def make_histogram(_image_path):
 
 def equalize_histogram(_image):
 
+    # normalizuje histogram slike
+    # kao argument prima crno-bijelu sliku (ne putanju)
+    # vraca crno-bijelu izjednacenu sliku
+
     # get image after histogram equalization
     _equalized_image = cv2.equalizeHist(_image)
 
@@ -32,6 +40,9 @@ def equalize_histogram(_image):
 
 
 def show_images(_image_path):
+
+    # 'stampa' nekoliko sliku na ekran radi uporedjivanja
+    # argument je putanja do slike
 
     _original_image = cv2.imread(_image_path)
     _grayscale_image = cv2.imread(_image_path, 0)
@@ -48,10 +59,21 @@ def show_images(_image_path):
 
 def threshold_image(_image):
 
+    # odvaja zgradu od pozadine
+    # argument je slika (ne putanja)
+    # vraca obradjenu sliku
+
+    # param 1 - image
+    # param 3 - metod
+    # param 5 - velicina bloka za pretragu (mora biti neparan broj veci od 1)
+    # param 6 - parametar koji utice na pretragu, proizvoljan
     _threshold_image = cv2.adaptiveThreshold(_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 3, 12)
     return _threshold_image
 
 def closing_image(_image):
+
+    # treba da ocisti sliku
+    # ne radi kako treba za sada
 
     _closing = cv2.morphologyEx(_image, cv2.MORPH_CLOSE, np.ones((3,3),np.uint8))
     return _closing
